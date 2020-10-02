f430eb2e7f5d5fdeb0f083c64a40d6b8611103fd
webermg <matthew.gerald.weber@gmail.com>
2020-10-02 16:42:30 -0700

    switched to promise syntax in init()

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
 const generateMarkdown = require("./utils/generateMarkdown");
+const questions = require("./utils/questions");
 
 const inq = require("inquirer");
 const fs = require("fs");
-const questions = require("./utils/questions")
+const util = require("util");
 
-
-
-// function to write README file
-function writeToFile(fileName, text) {
-    fs.writeFile(fileName,text,function(err) {
-        if(err) return console.log(err);
-    })
-    console.log("File Written");
-}
-
-//temp data
-let data = {
-    "title":"some Title",
-    "desc":"a project",
-    "sections": ["Installation","Usage","License","Contributing","Tests"],
-    "inst":"how to install",
-    "usage":"how to use",
-    "contr":"How to contribute\nanother line",
-    "tests":"npm run test",
-    "lic":"MIT",
-    "usern":"somedude",
-    "email":"somedude@dude.com"
-}
+const writeFile =  util.promisify(fs.writeFile);
 
 // function to initialize program
 function init() {
-    
-    const text = generateMarkdown(data);
-    writeToFile("output/README.md",text);
-
-    // inq.prompt(questions).then(function(response) {
-    //     const text = generateMarkdown(response);
-    //     writeToFile("output/README.md",text);
-    // })
+    inq.prompt(questions).then(function(response) {
+        return writeFile(`output/README_${Date.now()}.md`,generateMarkdown(response));
+    });
 }
-
-
 
 // function call to initialize program
 init();
